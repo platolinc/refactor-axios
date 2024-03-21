0f58d8b7f10f65df0cecdce3e0dbaccf664c5c82
platolinc <2686550623@qq.com>
2024-03-21 23:56:18 +0800

    feat: 拦截器-demo编写

--- src/types/index.ts
+++ src/types/index.ts
@@ -46,6 +46,11 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>
+    response: AxiosInterceptorManager<AxiosResponse>
+  }
+
   request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
   get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
