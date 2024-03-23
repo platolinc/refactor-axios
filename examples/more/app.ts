6d7b467a44127f9b13c15e8aed73245ee9c4fd19
platolinc <2686550623@qq.com>
2024-03-23 19:52:40 +0800

    feat: 自定义合法状态码
    提供一个配置允许用户自定义合法状态码规则

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -85,13 +85,30 @@ import NProgress from 'nprogress'
 // })
 
 
-axios.post('/more/post', {
-  a: 1
-}, {
-  auth: {
-    username: 'lch',
-    password: '123456'
+// axios.post('/more/post', {
+//   a: 1
+// }, {
+//   auth: {
+//     username: 'lch',
+//     password: '123456'
+//   }
+// }).then(res => {
+//   console.log(res)
+// })
+
+
+axios.get('/more/304').then(res => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e.message)
+})
+
+axios.get('/more/304', {
+  validateStatus(status) {
+    return status >= 200 && status < 400
   }
 }).then(res => {
   console.log(res)
+}).catch((e) => {
+  console.log(e.message)
 })
